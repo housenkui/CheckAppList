c4634aec706d46516bea33e780fd260131d74ca1
Daves_Code_Hou <840737320@qq.com>
2016-09-06 12:25:20 +0800

    Initial Commit

---
+++ CheckAppList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CheckAppList
+//
+//  Created by 二哈 on 16/9/6.
+//  Copyright © 2016年 侯森魁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CheckAppList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CheckAppList
+//
+//  Created by 二哈 on 16/9/6.
+//  Copyright © 2016年 侯森魁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
